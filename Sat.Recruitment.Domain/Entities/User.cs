639f95ef05cf275878a7c9553c69386614aab128
arenazl <arenazl@gmail.com>
2023-03-21 23:18:23 -0300

    Add files via upload

--- Sat.Recruitment.Domain/Entities/User.cs
+++ Sat.Recruitment.Domain/Entities/User.cs
@@ -12,44 +12,6 @@ namespace Sat.Recruitment.Domain.Entities
         public decimal Money { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime? UpdatedAt { get; set; }
-
-        public void ApplyAmountBasedInUserType()
-        {
-            var typeFactors = new Dictionary<UserType, decimal>
-        {
-            { UserType.Normal, 0.8m },
-            { UserType.SuperUser, 0.2m },
-            { UserType.Premium, 2m }
-        };
-
-            if (typeFactors.ContainsKey(UserType))
-            {
-                var factor = typeFactors[UserType];
-                Money = Money + (Money * factor);
-            }
-
-            if(UserType == UserType.Normal)
-                Money = Money > 100 ? Money + (Money * 12m) : Money;
-        }
-
-        public void NormalizeMail()
-        {
-            string email = Email.Trim();
-            if (!string.IsNullOrWhiteSpace(email))
-            {
-                string[] emailParts = email.Split('@');
-                if (emailParts.Length == 2)
-                {
-                    string username = emailParts[0].Replace(".", string.Empty);
-                    int plusIndex = username.IndexOf("+");
-                    if (plusIndex >= 0)
-                    {
-                        username = username.Remove(plusIndex);
-                    }
-                    Email = $"{username}@{emailParts[1]}";
-                }
-            }
-        }
     }
 
     public enum UserType
